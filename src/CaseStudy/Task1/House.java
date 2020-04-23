2d04f5f3ea7d66ca4b03f700432f097357a544ed
https://github.com/vongocquan/VoNgocQuan_Module2 <quanv4297@gmail.com>
2020-04-23 23:57:14 +0700

    nd

---
+++ src/CaseStudy/Task1/House.java
@@ -0,0 +1,57 @@
+package CaseStudy.Task1;
+
+public class House extends Services {
+    private String tieuChuanPhong;
+    private String moTaTienNghiKhac;
+    private String soTang;
+
+    public House() {
+    }
+
+    public String getTieuChuanPhong() {
+        return tieuChuanPhong;
+    }
+
+    public void setTieuChuanPhong(String tieuChuanPhong) {
+        this.tieuChuanPhong = tieuChuanPhong;
+    }
+
+    public String getMoTaTienNghiKhac() {
+        return moTaTienNghiKhac;
+    }
+
+    public void setMoTaTienNghiKhac(String moTaTienNghiKhac) {
+        this.moTaTienNghiKhac = moTaTienNghiKhac;
+    }
+
+    public String getSoTang() {
+        return soTang;
+    }
+
+    public void setSoTang(String soTang) {
+        this.soTang = soTang;
+    }
+
+    public House(String tieuChuanPhong, String moTaTienNghiKhac, String soTang) {
+        this.tieuChuanPhong = tieuChuanPhong;
+        this.moTaTienNghiKhac = moTaTienNghiKhac;
+        this.soTang = soTang;
+    }
+
+    public House(String tenDichVu, String dienTichSuDung, String chiPhiThue, String soLuongNguoiToiDa, String kieuThue, String tieuChuanPhong, String moTaTienNghiKhac, String soTang) {
+        super(tenDichVu, dienTichSuDung, chiPhiThue, soLuongNguoiToiDa, kieuThue);
+        this.tieuChuanPhong = tieuChuanPhong;
+        this.moTaTienNghiKhac = moTaTienNghiKhac;
+        this.soTang = soTang;
+    }
+
+    @Override
+    public String showInfor() {
+        return "House{" +
+                "tieuChuanPhong='" + tieuChuanPhong + '\'' +
+                ", moTaTienNghiKhac='" + moTaTienNghiKhac + '\'' +
+                ", soTang=" + soTang +
+                super.toString()+
+                '}';
+    }
+}
