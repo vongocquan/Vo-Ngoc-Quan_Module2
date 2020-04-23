7e24ac3cbfc4f6d27d74975df0e434f0f9979534
https://github.com/vongocquan/VoNgocQuan_Module2 <quanv4297@gmail.com>
2020-04-23 23:38:03 +0700

    nd

---
+++ Test.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args) {
+        List<String> arr = new ArrayList<>();
+        arr.add("vo");
+        arr.add("i");
+        arr.add("quan");
+        arr.clear();
+        System.out.print(arr.get(0));
+    }
+}
